883a9ecca2a64dc35fd8ac7fae60f2c17ba5fcad
Yuxuan Lee <s110519018@stu.ntue.edu.tw>
2022-08-04 11:06:09 +0800

    test: vue publicPath setting

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/2022Portfolio/'
-    : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/2022Portfolio/'
+  //   : './'
+  publicPath: './'
 });
